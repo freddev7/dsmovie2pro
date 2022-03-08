4046f0a54cd47ca910e9b6aced19e925a8b62648
Frederico <fredfrgs@gmail.com>
2022-03-08 12:46:44 -0300

    Project created

---
+++ backend/src/main/java/com/freddev7/dsmovie2pro/Dsmovie2proApplication.java
@@ -0,0 +1,13 @@
+package com.freddev7.dsmovie2pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsmovie2proApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsmovie2proApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/freddev7/dsmovie2pro/Dsmovie2proApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freddev7.dsmovie2pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsmovie2proApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
